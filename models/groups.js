4fc04f0f17a229539ccc546fe49ea260611fc41a
gauravUFL <gthapliyal@ufl.edu>
2020-07-29 02:55:53 -0400

    Added groups Schema

---
+++ models/groups.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const GroupSchema = new Schema({
+  course_name: {
+    type: String,
+    required: true,
+  },
+  member_list_names: {
+    type: String,
+    required: true,
+  },
+  member_list_emails: {
+    type: String,
+    required: true,
+  },
+  course_link: {
+    type: String,
+    required: true,
+  },
+  course_name: {
+    type: String,
+    required: true,
+  },
+  start_date: {
+    type: Date,
+    required: Date.now,
+  },
+  expected_end_date: {
+    type: Date,
+    required: Date.now,
+  },
+  assignments_list: {
+    type: String,
+    required: true,
+  },
+});
+module.exports = Groups = mongoose.model("groups", GroupSchema);
